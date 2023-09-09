639470ecbf3218de66b8bca9b9143209892c1110
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-09-09 18:15:17 +0500

    Initial commit

---
+++ exa-1_Start/src/main/java/com/example/exa1_start/Exa1StartApplication.java
@@ -0,0 +1,13 @@
+package com.example.exa1_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exa1StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exa1StartApplication.class, args);
+    }
+
+}
---
+++ exa-1_Start/src/test/java/com/example/exa1_start/Exa1StartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exa1_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exa1StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
