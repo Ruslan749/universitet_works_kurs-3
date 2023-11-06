36e0203ba19ea7b956335b82db569076386177fa
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-11-06 13:09:50 +0500

    exe_7 Thymeleaf

--- exe_7-thymelife/src/main/java/com/example/exe_6springwebserver/dao/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.exe_6springwebserver.dao;
-
-import com.example.exe_6springwebserver.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student,Long> {
-}
